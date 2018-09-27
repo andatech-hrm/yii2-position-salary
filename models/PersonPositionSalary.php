48e2ba6195c476112e9ce992cd8d7c39b078d7b5
Ahamad-PC <ahamad.jedu@gmail.com>
2018-09-28 06:40:51 +0700

    add update position on person

--- models/PersonPositionSalary.php
+++ models/PersonPositionSalary.php
@@ -3,17 +3,16 @@
 namespace andahrm\positionSalary\models;
 
 use Yii;
-
 use yii\behaviors\BlameableBehavior;
 use yii\behaviors\TimestampBehavior;
 use yii\helpers\ArrayHelper;
-
 use andahrm\person\models\Person;
 use andahrm\edoc\models\Edoc;
 use andahrm\structure\models\Position;
-
 use andahrm\datepicker\behaviors\DateBuddhistBehavior;
 use andahrm\datepicker\behaviors\YearBuddhistBehavior;
+use andahrm\datepicker\components\ThaiYearFormatter;
+
 /**
  * This is the model class for table "person_position_salary".
  *
@@ -35,22 +34,20 @@ use andahrm\datepicker\behaviors\YearBuddhistBehavior;
  * @property Person $user
  * @property Position $position
  */
-class PersonPositionSalary extends \yii\db\ActiveRecord
-{
+class PersonPositionSalary extends \yii\db\ActiveRecord {
+
     /**
      * @inheritdoc
      */
-    public static function tableName()
-    {
+    public static function tableName() {
         return 'person_position_salary';
     }
-  
-  /**
+
+    /**
      * @inheritdoc
      */
-     function behaviors()
-    {
-        
+    function behaviors() {
+
         return [
             [
                 'class' => BlameableBehavior::className(),
@@ -68,14 +65,13 @@ class PersonPositionSalary extends \yii\db\ActiveRecord
     /**
      * @inheritdoc
      */
-    public function rules()
-    {
+    public function rules() {
         return [
             //[['user_id', 'position_id', 'adjust_date', 'title', 'status', 'level', 'salary','person_type_id' ], 'required'],
-            [['user_id', 'position_id', 'adjust_date', 'status', 'level', 'salary','person_type_id' ], 'required'],
-            [['user_id', 'position_id','position_type_id','position_level_id', 'edoc_id', 'created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
-            [['adjust_date','status','select_status'], 'safe'],
-            [['step_adjust', 'salary','step'], 'number'],
+            [['user_id', 'position_id', 'adjust_date', 'status', 'level', 'salary', 'person_type_id'], 'required'],
+            [['user_id', 'position_id', 'position_type_id', 'position_level_id', 'edoc_id', 'created_at', 'created_by', 'updated_at', 'updated_by'], 'integer'],
+            [['adjust_date', 'status', 'select_status'], 'safe'],
+            [['step_adjust', 'salary', 'step'], 'number'],
             //[['title'], 'string', 'max' => 255],
             [['level'], 'string', 'max' => 15],
             //[['step'], 'string', 'max' => 4],
@@ -88,8 +84,7 @@ class PersonPositionSalary extends \yii\db\ActiveRecord
     /**
      * @inheritdoc
      */
-    public function attributeLabels()
-    {
+    public function attributeLabels() {
         return [
             'user_id' => Yii::t('andahrm/position-salary', 'User ID'),
             'position_old_id' => Yii::t('andahrm/position-salary', 'Position Old ID'),
@@ -98,18 +93,16 @@ class PersonPositionSalary extends \yii\db\ActiveRecord
             'adjust_date' => Yii::t('andahrm/position-salary', 'Adjust Date'),
             'title' => Yii::t('andahrm/position-salary', 'Title'),
             'title-list' => Yii::t('andahrm/position-salary', 'Title List'),
-            
             'select_status' => Yii::t('andahrm/position-salary', 'Status'),
             'status' => Yii::t('andahrm/position-salary', 'Status'),
             'step' => Yii::t('andahrm/position-salary', 'Step'),
-            'step_adjust' => Yii::t('andahrm/position-salary', 'Step Adjust'), 
+            'step_adjust' => Yii::t('andahrm/position-salary', 'Step Adjust'),
             'level' => Yii::t('andahrm/position-salary', 'Level'),
             'salary' => Yii::t('andahrm/position-salary', 'Salary'),
             'created_at' => Yii::t('andahrm', 'Created At'),
             'created_by' => Yii::t('andahrm', 'Created By'),
             'updated_at' => Yii::t('andahrm', 'Updated At'),
             'updated_by' => Yii::t('andahrm', 'Updated By'),
-            
             'person_type_id' => Yii::t('andahrm/structure', 'Person Type'),
             'new_step' => Yii::t('andahrm/position-salary', 'New Step'),
             'new_level' => Yii::t('andahrm/position-salary', 'New Level'),
@@ -117,161 +110,149 @@ class PersonPositionSalary extends \yii\db\ActiveRecord
             'new_position_id' => Yii::t('andahrm/position-salary', 'New Position'),
             'start_date' => Yii::t('andahrm/position-salary', 'Start Date'),
             'end_date' => Yii::t('andahrm/position-salary', 'End Date'),
-            
             'position_type_id' => Yii::t('andahrm/structure', 'Position Type'),
             'position_level_id' => Yii::t('andahrm/structure', 'Position Level'),
         ];
     }
-    
+
     public $position_old_id;
     public $person_type_id;
     public $select_status;
-    
-   const SCENA_STATUS2 = 'status2'; 
-   const SCENA_STATUS3 = 'status3'; 
-   const SCENA_STATUS4= 'status4'; 
-    
-    public function scenarios(){
-      $scenarios = parent::scenarios();
-      
-      $scenarios['new-person'] = [
-          //'user_id',
-              'position_id', 'edoc_id', 'salary', 'user_id', 'level','status','adjust_date'
-          ];
-          
-      $scenarios['insert'] = [
-         'user_id' ,'salary', 'position_id', 'level','status',
-         //'new_level', 'new_salary', 'new_position_id'
-          ];
-        $scenarios['update'] = [
-            'adjust_date', 'salary', 'position_id', 'level','status','edoc_id', 'step', 'salary','updated_at','updated_by'
-          ];
-      $scenarios[self::SCENA_STATUS2] = [
-         'user_id' ,'salary', 'position_id', 'level','status',
-         'new_level','step_adjust', 'new_salary'
-          ];
-      $scenarios[self::SCENA_STATUS3] = [
-         'user_id' ,'salary', 'position_id', 'level','status',
-         'new_position_id', 
-         'start_date', 'end_date'
-          ];
-      $scenarios[self::SCENA_STATUS4] = ['user_id' ,'salary', 'position_id', 'level','status' ];
-      
-      return $scenarios;
-    }
-    
-    /** 
-    * @inheritdoc 
-    * @return PersonContractQuery the active query used by this AR class. 
-    */ 
-   public static function find() 
-   { 
-       return new PersonPositionSalaryQuery(get_called_class()); 
-   }
-    
-    
 
-   const STATUS_FIRST_TIME = 1; #บรรจุแรกเข้า
-   const STATUS_ADJUST = 2; #ปรับเงินเดือน
-   const STATUS_MOVE = 3; #ย้ายสายงาน
-   const STATUS_LEAVE = 4; #สินสุดการจ้าง
-   const STATUS_TRANSFER = 5; #สินสุดการจ้าง
-   
-  
+    const SCENA_STATUS2 = 'status2';
+    const SCENA_STATUS3 = 'status3';
+    const SCENA_STATUS4 = 'status4';
+
+    public function scenarios() {
+        $scenarios = parent::scenarios();
+
+        $scenarios['new-person'] = [
+            //'user_id',
+            'position_id', 'edoc_id', 'salary', 'user_id', 'level', 'status', 'adjust_date'
+        ];
+
+        $scenarios['insert'] = [
+            'user_id', 'salary', 'position_id', 'level', 'status',
+                //'new_level', 'new_salary', 'new_position_id'
+        ];
+        $scenarios['update'] = [
+            'adjust_date', 'salary', 'position_id', 'level', 'status', 'edoc_id', 'step', 'salary', 'updated_at', 'updated_by'
+        ];
+        $scenarios[self::SCENA_STATUS2] = [
+            'user_id', 'salary', 'position_id', 'level', 'status',
+            'new_level', 'step_adjust', 'new_salary'
+        ];
+        $scenarios[self::SCENA_STATUS3] = [
+            'user_id', 'salary', 'position_id', 'level', 'status',
+            'new_position_id',
+            'start_date', 'end_date'
+        ];
+        $scenarios[self::SCENA_STATUS4] = ['user_id', 'salary', 'position_id', 'level', 'status'];
+
+        return $scenarios;
+    }
+
+    /**
+     * @inheritdoc 
+     * @return PersonContractQuery the active query used by this AR class. 
+     */
+    public static function find() {
+        return new PersonPositionSalaryQuery(get_called_class());
+    }
+
+    const STATUS_FIRST_TIME = 1; #บรรจุแรกเข้า
+    const STATUS_ADJUST = 2; #ปรับเงินเดือน
+    const STATUS_MOVE = 3; #ย้ายสายงาน
+    const STATUS_LEAVE = 4; #สินสุดการจ้าง
+    const STATUS_TRANSFER = 5; #สินสุดการจ้าง
+
     public static function itemsAlias($key) {
         $items = [
             'status' => [
                 self::STATUS_FIRST_TIME => Yii::t('andahrm/position-salary', 'First time'),
-                self::STATUS_ADJUST => Yii::t('andahrm/position-salary',  'Adjust salary'),
-                self::STATUS_MOVE => Yii::t('andahrm/position-salary',  'Move line'),
-                self::STATUS_LEAVE => Yii::t('andahrm/position-salary',  'Leave'),
+                self::STATUS_ADJUST => Yii::t('andahrm/position-salary', 'Adjust salary'),
+                self::STATUS_MOVE => Yii::t('andahrm/position-salary', 'Move line'),
+                self::STATUS_LEAVE => Yii::t('andahrm/position-salary', 'Leave'),
             ],
         ];
         return ArrayHelper::getValue($items, $key, []);
     }
-    
+
     public function getStatusLabel() {
         return ArrayHelper::getValue($this->getItemStatus(), $this->status);
     }
 
     public static function getItemStatus() {
-          return self::itemsAlias('status');
-     }
-    
+        return self::itemsAlias('status');
+    }
+
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getEdoc()
-    {
+    public function getEdoc() {
         return $this->hasOne(Edoc::className(), ['id' => 'edoc_id']);
     }
 
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getUser()
-    {
+    public function getUser() {
         return $this->hasOne(Person::className(), ['user_id' => 'user_id']);
     }
 
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getPosition()
-    {
+    public function getPosition() {
         return $this->hasOne(Position::className(), ['id' => 'position_id']);
     }
-    
-    public function getPersonPosition()
-    {
+
+    public function getPersonPosition() {
         return $this->hasOne(PersonPosition::className(), ['user_id' => 'user_id']);
     }
-    
-    public function getAssessment()
-    {
+
+    public function getAssessment() {
         return $this->hasOne(Assessment::className(), ['user_id' => 'user_id']);
     }
-    
-    public static function getRangeStep(){
-        $range = range(0.5,2,0.5);
-        return array_combine($range,$range);
+
+    public static function getRangeStep() {
+        $range = range(0.5, 2, 0.5);
+        return array_combine($range, $range);
     }
-    
-    public function getTitleStep(){
-         return $this->title.($this->step_adjust*1>0?
-                ' เลื่อน '.$this->step_adjust .' ขั้น'
-                :'');
+
+    public function getTitleStep() {
+        return $this->title . ($this->step_adjust * 1 > 0 ?
+                ' เลื่อน ' . $this->step_adjust . ' ขั้น' : '');
     }
-    public function getPositionTitleCode(){
+
+    public function getPositionTitleCode() {
         return $this->position->titleCode;
     }
-    
+
     #check exists record
-    public function getExists(){
+
+    public function getExists() {
         return self::find()->where([
-            'user_id'=>$this->user_id,
-            'position_id'=>$this->position_id,
-            'edoc_id'=>$this->edoc_id,
-        ])->exists();
+                    'user_id' => $this->user_id,
+                    'position_id' => $this->position_id,
+                    'edoc_id' => $this->edoc_id,
+                ])->exists();
     }
-    
-    public function getPositionLevel()
-    {
+
+    public function getPositionLevel() {
         return $this->hasOne(PositionLevel::className(), ['id' => 'position_level_id']);
     }
-    
 
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getPersonType()
-    {
+    public function getPersonType() {
         return $this->hasOne(PersonType::className(), ['id' => 'person_type_id']);
     }
-    
-    public function getTitle(){
+
+    public function getTitle() {
         $str = $this->position->title;
-         switch($this->status){
+        switch ($this->status) {
             // case self::STATUS_FIRST_TIME:
             //      $str = $this->position->title;
             //     //exit();
@@ -289,32 +270,48 @@ class PersonPositionSalary extends \yii\db\ActiveRecord
             //     $arr_str = array_filter($arr_str);
             //     $str = implode("<br/>",$arr_str);
             // break;
-            
+
             default:
                 $arr_str[] = $this->position->title;
                 $arr_str[] = $this->edoc->title;
                 $arr_str = array_filter($arr_str);
-                $str = implode("<br/>",$arr_str);
-            break;
+                $str = implode("<br/>", $arr_str);
+                break;
         }
         return $str;
     }
-    
-    
+
     public function afterSave($insert, $changedAttributes) {
         parent::afterSave($insert, $changedAttributes);
-        
-        $model = Person::findOne($this->user_id);
+        self::updatePostion($this->user_id);
+    }
+
+    public static function updatePostion($user_id) {
+        $model = Person::findOne($user_id);
+        $model->scenario = Person::SCENARIO_UPDATE_POSITION;
+       // $model->birthday = Yii::$app->formatter->asDate($model->birthday);
+       // $model->birthday = \DateTime::createFromFormat('Y-m-d', $model->birthday);
+
         $position = $model->positionLast;
-        $model->position_id = $position?$position->id:null;
-        if($model->save(false)){
+        $model->position_id = $position ? $position->id : null;
+        //echo "Before<br/>";
+        //echo $model->birthday . "<br/>";
+        //echo $model->position_id;
+
+        //print_r($model->attributes);
+
+        if ($model->save()) {
+            //echo "After<br/>";
+           // print_r($model->attributes);
+            //echo $model->birthday . "<br/>";
+           // echo $model->position_id;
+            //exit();
             return true;
-        }else{
+        } else {
             echo "Error:afterSave<br/>";
             print_r($model->getErrors());
             exit();
         }
-        
     }
-    
+
 }
